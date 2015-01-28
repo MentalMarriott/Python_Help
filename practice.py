608a7bbd451c0eda9d9ef42c739ce2846cc66215
mentalmarriott <firemazza@gmail.com>
2015-01-28 15:26:13 +0000

    Added information on resources used

--- practice.py
+++ practice.py
@@ -5,6 +5,8 @@ Created on Wed Jan 28 14:41:04 2015
 @author: marriott
 """
 
+# Help and guidence from http://www.swaroopch.com/notes/python/
+
 print "Hello World" # this will display test in the " " in the console
 
 print 'Single quotes can also be used'  #This preserves all white space and tabs
@@ -23,5 +25,13 @@ age = 24
 name = "Chris"        # These are both variables
 
 # Showing that using " and ' result in same output
-print "{0} is {1} years old, {0}".format(name, age)
-print '{0} is {1} years old, {0}'.format(name, age)
+print "{0} is {1} years old".format(name, age)
+print '{0} is {1} years old'.format(name, age)
+# The {0} refers to the first argument in the format function and
+# the {1} refers to the second argument
+
+# Doing the same as above but with concatination
+print name + ' is ' + str(age) + ' years old.'  
+# The str() function allows the age which is a number to be interpreted as
+# a string, this is uglier and more error prone
+
